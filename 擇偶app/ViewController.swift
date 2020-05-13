42a1f43681a3b7ab645a440682ed462aa414014e
stoller <stoller@stollerde-macbook-pro.local>
2020-05-13 21:36:24 +0800

    Initial Commit

---
+++ 擇偶app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  擇偶app
+//
+//  Created by stoller on 2020/5/13.
+//  Copyright © 2020年 cgh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
