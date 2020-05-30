9b7ac1479c5dfa060eab5c9608d91178358caec2
stoller <stoller@stollerde-macbook-pro.local>
2020-05-30 18:15:33 +0800

    finish

--- 擇偶app/ViewController.swift
+++ 擇偶app/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var bloodTypeSegment: UISegmentedControl!
+    
+    @IBOutlet weak var animalSwitch: UISwitch!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    
+    @IBOutlet weak var heightNumber: UILabel!
+    
+    @IBOutlet weak var cryImageView: UIImageView!
+    
+    @IBOutlet weak var bingoImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func height(_ sender: UISlider) {
+        heightNumber.isHidden = false
+        heightNumber.text = String(Int(sender.value))
+    }
+    
+    @IBAction func okButton(_ sender: AnyObject) {
+        let star = textField.text!
+        let blood = bloodTypeSegment.selectedSegmentIndex
+        let animalLover = animalSwitch.isOn
+        let height = Int(heightSlider.value)
+        
+        if star == "射手座" && blood == 0 && animalLover && height == 158 {
+            bingoImage.isHidden = false
+            cryImageView.isHidden = true
+            
+        } else {
+            bingoImage.isHidden = true
+            cryImageView.isHidden = false
+            
+        }
+
+        
+    }
 }
 
